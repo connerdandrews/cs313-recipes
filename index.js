b66ba4a8f3eaf070bdef2af08e833b837b39fcb7
Conner Andrews <and16054@byui.edu>
2020-11-28 14:03:11 -0700

    Debugging still

--- index.js
+++ index.js
@@ -38,10 +38,14 @@ function getIngredients(request, response) {
 }
 
 function addIngredients(request, response) {
-    var ing_name = request.query.ingredient_name;
+    const ing_name = request.query.ingredient_name;
     const exp_day = request.query.expiration_day;
     const exp_month = request.query.expiration_month;
     const exp_year = request.query.expiration_year;
+    console.log("Ingredient name:" + ing_name);
+    console.log("Exp day: " + exp_day);
+    console.log("Exp month: " + exp_month);
+    console.log("Exp year: " + exp_year);
     const sql2 = 'INSERT INTO ingredient (ingredient_name, expiration_day, expiration_month, expiration_year) VALUES(\''+ ing_name + '\', ' + exp_day + ', ' + exp_month + ', ' + exp_year + ')';
     pool.query(sql2, function(err, result) {
         if (err) {
